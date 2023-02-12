96604410aa8eff12686b97a8bcf5b2d0f3cb689a
anna <annastacia78@gmail.com>
2023-02-12 06:52:03 -0800

    js html amendments

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,6 +4,7 @@ $(function () {
   let temp = [];
   let text = [];
   $(':checkbox').on('click', function () {
+    console.log();
     if (clicked.indexOf(this['dataset']['id']) === -1) {
       clicked.push(this['dataset']['id']);
     } else {
@@ -17,6 +18,7 @@ $(function () {
     }
     for (let item of $('.popover li input')) {
       if (clicked.indexOf(item['dataset']['id']) !== -1) {
+        console.log('yay');
         text.push(item['dataset']['name']);
       }
     }
@@ -32,17 +34,5 @@ $(function () {
     } else {
       $('div#api_status').removeClass('available');
     }
-  });
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    data: '{}',
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function (data) {
-      for (let place of data) {
-        $('.places').append('<article><div class="title"><h2>' + place['name'] + '</h2><div class="price_by_night">' + place['price_by_night'] + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place['max_guest'] + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place['number_rooms'] + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place['number_bathrooms'] + ' Bathroom</div></div><div class="user"><div class="description">' + place['description'] + '</div></article>');
-      }
-    }
-  });
+  })
 });
